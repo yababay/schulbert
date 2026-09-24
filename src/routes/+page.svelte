842c51e7395d69db0776da9a51c6ee112c4f7106
Михаил Беляков <bigbelk@live.ru>
2026-09-25 02:03:57 +0300

    ok

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { state, actions } from './main.svelte';
+	import { PUBLIC_PAGE_TITLE } from '$env/static/public';
+	
+	onMount(() => {
+		
+	});
+</script>
+
+<div class="container">
+	<h3>{PUBLIC_PAGE_TITLE}</h3>
+</div>
+
+<style>
+
+</style>
---
+++ src/routes/main.svelte.ts
@@ -0,0 +1,8 @@
+export const state = $state({
+
+});
+
+
+export const actions = {
+
+};
