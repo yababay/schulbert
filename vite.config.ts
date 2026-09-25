58ea10a8e3a47f7aa02496caa708d8ca50415b2e
Михаил Беляков <bigbelk@live.ru>
2026-09-25 10:35:09 +0300

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,19 @@ export default defineConfig({
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
 		})
-	]
+	],
+	server: {
+			proxy: {
+					// Перехватываем все запросы, которые начинаются с /api
+					'/api': {
+							// Замените на реальный хост вашего API-шлюза в Яндекс Облаке
+							target: 'http://schulbert/', 
+							changeOrigin: true,
+							// Опционально: если ваш шлюз на бэкенде ожидает путь без префикса /api,
+							// можно его вырезать. Но если в шлюзе путь так и прописан (/api/generate), 
+							// то строчку ниже (rewrite) добавлять НЕ НАДО.
+							// rewrite: (path) => path.replace(/^\/api/, '')
+					}
+			}
+	}	
 });
